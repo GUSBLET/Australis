f0553a87af811a61d7ffdc13960ef117a10a5e59
Chykalov Denis <denyschk@gmail.com>
2022-09-25 22:45:11 +0300

    Add choose of language

--- Program.cs
+++ Program.cs
@@ -1,14 +1,42 @@
+using Microsoft.AspNetCore.Localization;
+using System.Globalization;
+
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddControllersWithViews();
+
+builder.Services.AddLocalization(options =>
+{
+    options.ResourcesPath = "Resources";
+});
+
+builder.Services.AddControllersWithViews()
+    .AddViewLocalization();
+
+builder.Services.Configure<RequestLocalizationOptions>(options =>
+{
+    options.DefaultRequestCulture = new RequestCulture("uk-UA");
+
+    var cultures = new CultureInfo[]
+    {
+        new CultureInfo("en-US"),
+        new CultureInfo("uk-UA"),
+    };
+
+    options.SupportedCultures = cultures;
+    options.SupportedUICultures = cultures;
+});
+
+
 var app = builder.Build();
 
 app.UseDeveloperExceptionPage();
 app.UseStaticFiles();
 app.UseRouting();
 app.UseAuthorization();
+
+app.UseRequestLocalization();
 app.UseEndpoints(endpoints =>
     {
-        endpoints.MapControllerRoute(
+    endpoints.MapControllerRoute(
                    name: "default",
                    pattern: "{controller=Home}/{action=Index}/{id?}");
     });
